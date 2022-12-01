91eb2bbaed76e559c502674a3047ac530aa5f83e
Felipe C. Minuzzi <feminuzzi@hotmail.com>
2022-12-01 15:37:01 -0300

    create derivatives source

--- src/derivatives.py
+++ src/derivatives.py
@@ -26,20 +26,71 @@ class FirstOrder:
 
         return diff
 
-    def differentiateSecondFO(self, x):
+    def differentiateSecondFO(self, x, y, h):
         """
         Compute first order second derivative using finite difference method
         """
 
-    def differentiateThirdFO(self, x):
+        num_variables = x.shape[0]
+        data_dim      = x.shape[-1]
+        diff          = np.empty([num_variables, data_dim])
+
+        for j in range(num_variables):
+
+            diff[j, 0]  = (y[j,2] - 2*y[j, 1] + y[j,0])/h**2
+            diff[j, -1] = (y[j,-1] - 2*y[j,-2] + y[j, -3])/h**2
+
+            for i in range(1,data_dim-1):
+                diff[j,i] = (y[j,i+1] - 2*y[j, i] +  y[j, i-1])/h**2
+
+        return diff
+
+    def differentiateThirdFO(self, x, y, h):
         """
         Compute first order third derivative using finite difference method
         """
 
-    def differentiateFourthFO(self, x):
+        num_variables = x.shape[0]
+        data_dim      = x.shape[-1]
+        diff          = np.empty([num_variables, data_dim])
+
+        for j in range(num_variables):
+
+            diff[j, 0]  = (y[j,3] - 3*y[j, 2] + 3*y[j, 1] - y[j,0])/h**3
+            diff[j, 1]  = (y[j,4] - 3*y[j, 3] + 3*y[j, 2] - y[j,1])/h**3
+
+            diff[j, -1] = (y[j,-1] - 3*y[j,-2] + 3*y[j, -3] - y[j, -4])/h**3
+            diff[j, -2] = (y[j,-2] - 3*y[j,-3] + 3*y[j, -4] - y[j, -5])/h**3
+
+            for i in range(2,data_dim-2):
+                diff[j,i] = (y[j,i+2] - 2*y[j,i+1] + 2*y[j, i-1] -y[j,i-2])/(2*(h**3))
+
+        return diff
+
+
+    def differentiateFourthFO(self, x, y, h):
         """
         Compute first order fourth derivative using finite difference method
         """
+
+        num_variables = x.shape[0]
+        data_dim      = x.shape[-1]
+        diff          = np.empty([num_variables, data_dim])
+
+        for j in range(num_variables):
+
+            diff[j, 0]  = (y[j,4] - 4*y[j, 3] + 6*y[j, 2] - 4*y[j,1] + y[j,0])/h**4
+            diff[j, 1]  = (y[j,5] - 4*y[j, 4] + 6*y[j, 3] - 4*y[j,2] + y[j,1])/h**4
+            diff[j, 2]  = (y[j,6] - 4*y[j, 5] + 6*y[j, 4] - 4*y[j,3] + y[j,2])/h**4
+
+            diff[j, -1] = (y[j,-1] - 4*y[j,-2] + 6*y[j, -3] - 4*y[j,-4] + y[j,-5])/h**4
+            diff[j, -2] = (y[j,-2] - 4*y[j,-3] + 6*y[j, -4] - 4*y[j,-5] + y[j,-6])/h**4
+            diff[j, -3] = (y[j,-3] - 4*y[j,-4] + 6*y[j, -5] - 4*y[j,-6] + y[j,-7])/h**4
+
+            for i in range(3,data_dim-2):
+                diff[j,i] = (y[j,i+2] - 4*y[j,i+1] + 6*y[j,i] - 4*y[j, i-1] +  y[j, i-2])/h**4
+
+        return diff
 
 class SecondOrder:
     def __init__(self):
