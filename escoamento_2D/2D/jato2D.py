20a16eb1fd22d8ae0ca64cbd332e5f026a1ebdd5
felipeminuzzi <feminuzzi@hotmail.com>
2024-11-12 17:45:57 -0300

    fix: flow running

--- escoamento_2D/2D/jato2D.py
+++ escoamento_2D/2D/jato2D.py
@@ -6,7 +6,7 @@ def plot_heatmap(u_k, x, y):
   plt.clf()
   plt.xlabel('x')
   plt.ylabel('y')
-  plt.pcolormesh(x,y,u_k, cmap=plt.cm.jet, vmin=0, vmax = 150)
+  plt.pcolormesh(x,y,u_k, cmap=plt.cm.jet)
   plt.colorbar()
 
 ##################################################################
@@ -15,18 +15,16 @@ def plot_heatmap(u_k, x, y):
 
 ni     = 151
 nj     = 51
-nt     = 100
+nt     = 1000
 re     = 900.
-dt     = 1.e-5
-dt2    = dt/2
+dt     = 1.e-3
 L      = 5.
 H      = 1.
 sbr    = 0.99
 roh2o  = 1.0  #g/cm3
 mih2o  = 0.01 #g/cm s
-sbr    = 0.99
 
-initial_cond  = 0
+initial_cond  = 1
 cc_bottom     = 0
 cc_top        = 0
 cc_left       = 1
@@ -48,100 +46,99 @@ xi     = np.linspace(0,L, ni)
 yi     = np.linspace(0,H, nj)
 
 ##################################################################
-#!		      Condições iniciais e de contorno	                 #
+#!		       Condições iniciais e de contorno	                 #
 ##################################################################	
 
-u  = np.zeros((nt, ni, nj))
-v  = np.zeros((nt, ni, nj))
-p  = np.zeros((nt, ni, nj))
-d1 = np.zeros((nt, ni, nj))
-
-# convect_x = np.zeros((nt, ni, nj))
-# convect_y = np.zeros((nt, ni, nj))
-# difus_x   = np.zeros((nt, ni, nj))
-# difus_y   = np.zeros((nt, ni, nj))
-# dpdx      = np.zeros((nt, ni, nj))
-# dpdy      = np.zeros((nt, ni, nj))
+u  = np.zeros((nt, nj, ni))
+v  = np.zeros((nt, nj, ni))
+p  = np.zeros((nt, nj, ni))
+d1 = np.zeros((nt, nj, ni))
 
 #condição inicial
 u[0,:,:] = initial_cond
-v[0,:,:] = initial_cond
+v[0,:,:] = 0
+p[0,:,:] = initial_cond
 #condição de contorno x
 u[:,0,:]  = cc_top
-u[:,ni-1,:] = cc_bottom
+u[:,nj-1,:] = cc_bottom
 u[:,:,0]  = cc_left
-u[:,:,nj-1] = cc_right
+u[:,:,ni-1] = cc_right
 
 #condição de contorno y
 v[:,0,:]  = cc_top
-v[:,ni-1,:] = cc_top
+v[:,nj-1,:] = cc_top
 v[:,:,0]  = cc_top
-v[:,:,nj-1] = cc_top
+v[:,:,ni-1] = cc_top
 
 ##################################################################
 #!		          Quantidade de movimento    	                 #
 ##################################################################	
 
-for it in tqdm(range(nt-1)):
+for it in range(nt-1):
 
     for i in range(1,ni-1):
         for j in range(1,nj-1):
 
-            convect_x     = (u[it,i+1,j]*u[it,i+1,j] - u[it,i-1,j]*u[it,i-1,j])/(2*dx) + \
-                            (u[it,i,j+1]*v[it,i,j+1] - u[it,i,j-1]*v[it,i,j-1])/(2*dy)
+            convect_x     = (u[it,j,i+1]*u[it,j,i+1] - u[it,j,i-1]*u[it,j,i-1])/(2*dx) + (u[it,j+1,i]*v[it,j+1,i] - u[it,j-1,i]*v[it,j-1,i])/(2*dy) 
+            convect_y     = (v[it,j+1,i]*v[it,j+1,i] - v[it,j-1,i]*v[it,j-1,i])/(2*dy) + (v[it,j,i+1]*u[it,j,i+1] - v[it,j,i-1]*u[it,j,i-1])/(2*dx)
             
-            convect_y     = (v[it,i,j+1]*v[it,i,j+1] - v[it,i,j-1]*v[it,i,j-1])/(2*dy) + \
-                            (v[it,i+1,j]*u[it,i+1,j] - v[it,i-1,j]*u[it,i-1,j])/(2*dx)
+            difus_x       = (u[it,j,i+1] - 2*u[it,j,i] + u[it,j,i-1])/dx2 + (u[it,j+1,i] - 2*u[it,j,i] + u[it,j-1,i])/dy2            
+            difus_y       = (v[it,j,i+1] - 2*v[it,j,i] + v[it,j,i-1])/dx2 + (v[it,j+1,i] - 2*v[it,j,i] + v[it,j-1,i])/dy2   
+
+            dudx          = (u[it,j,i+1] - u[it,j,i-1])/(2*dx)
+            dvdy          = (v[it,j+1,i] - v[it,j-1,i])/(2*dy)         
             
-            difus_x       = (u[it,i+1,j] - 2*u[it,i,j] + u[it,i-1,j])/dx2 + \
-                            (u[it,i,j+1] - 2*u[it,i,j] + u[it,i,j-1])/dy2
-            
-            difus_y       = (v[it,i+1,j] - 2*v[it,i,j] + v[it,i-1,j])/dx2 + \
-                            (v[it,i,j+1] - 2*v[it,i,j] + v[it,i,j-1])/dy2   
+            d1[it,j,i]    = dudx + dvdy
 
-            dudx          = (u[it,i+1,j] - u[it,i-1,j])/(2*dx)
-            dvdy          = (v[it,i,j+1] - v[it,i,j-1])/(2*dy)         
-            
-            d1[it,i,j]    = dudx + dvdy
+            dpdx          = (p[it,j,i+1] - p[it,j,i-1])/(2*dx) 
+            dpdy          = (p[it,j+1,i] - p[it,j-1,i])/(2*dy) 
 
-            dpdx          = (p[it,i+1,j] - p[it,i-1,j])/(2*dx) 
-            dpdy          = (p[it,i,j+1] - p[it,i,j-1])/(2*dy) 
+            u[it+1,j,i]   = u[it,j,i] + dt*(-convect_x - dpdx + (1/re)*difus_x)
+            v[it+1,j,i]   = v[it,j,i] + dt*(-convect_y - dpdy + (1/re)*difus_y)
 
-            u[it+1,i,j]   = u[it,i,j] + dt*(-convect_x - dpdx + (1/re)*difus_x)
-            v[it+1,i,j]   = v[it,i,j] + dt*(-convect_y - dpdy + (1/re)*difus_y)
-
-
-    # for i in range(1,ni-1):
-    #     for j in range(1,nj-1):
-            
-    #         d2d1dx2       = (d1[it,i+1,j] - 2*d1[it,i,j] + d1[it,i-1,j])/dx2
-    #         d2d1dy2       = (d1[it,i,j+1] - 2*d1[it,i,j] + d1[it,i,j-1])/dy2
-
-    #         dpx           = p[it,i+1,j] + p[it,i-i,j]
-    #         dpy           = p[it,i,j+1] + p[it,i,j-1]
-
-    #         d2udx2        = (u[it,i+1,j]*u[it,i+1,j] - 2*u[it,i,j]*u[it,i,j] + u[it,i-1,j]*u[it,i-1,j])/dx2
-    #         d2vdy2        = (v[it,i,j+1]*v[it,i,j+1] - 2*v[it,i,j]*v[it,i,j] + v[it,i,j-1]*v[it,i,j-1])/dy2
-    #         d2uvdxy2      = (u[it,i+1,j+1]*v[it,i+1,j+1] - u[it,i+1,j-1]*v[it,i+1,j-1] - u[it,i-1,j+1]*v[it,i-1,j+1] + u[it,i-1,j-1]*v[it,i-1,j-1])/(4*dx*dy)
-
-    #         conv_esp_pr   = d2udx2 + d2vdy2 + d2uvdxy2
-
-    #         p[it,i,j]   = ((dx2*dy2)/(2*(dy2 + dx2)))*(dpx/dx2 + dpy/dy2 - (1/re)*(d2d1dx2 + d2d1dy2) + (1/dt)*d1[it,i,j] + conv_esp_pr) 
-    #         #p[it,i,j]   = sbr*p[it,i,j] + (1.- sbr)*p[it+1,i,j]
     
-    # for j in range(0,ni):
-    #    p[it,j,0]       = p[it,j,2]
-    #    p[it,j,nj-1]    = 1. 
+    for i in range(1,ni-1):
+        for j in range(1,nj-1):
+            
+            d2d1dx2       = (d1[it,j,i+1] - 2*d1[it,j,i] + d1[it,j,i-1])/dx2
+            d2d1dy2       = (d1[it,j+1,i] - 2*d1[it,j,i] + d1[it,j-1,i])/dy2
 
-    # for i in range(0,nj):
-    #     p[it,0,i]      = .75*p[it,1,i] + .25*p[it,2,i]
-    #     p[it,ni-1,i]   = .75*p[it,ni-2,i] + .25*p[it,ni-3,i]
+            dpx           = (p[it,j,i+1] + p[it,j,i-1])/dx2
+            dpy           = (p[it,j+1,i] + p[it,j-1,i])/dy2 
+            dp            = dpx + dpy
+
+            d2udx2        = (u[it,j,i+1]*u[it,j,i+1] - 2*u[it,j,i]*u[it,j,i] + u[it,j,i-1]*u[it,j,i-1])/dx2
+            d2vdy2        = (v[it,j+1,i]*v[it,j+1,i] - 2*v[it,j,i]*v[it,j,i] + v[it,j-1,i]*v[it,j-1,i])/dy2
+            d2uvdxy2      = (u[it,j+1,i+1]*v[it,j+1,i+1] - u[it,j-1,i+1]*v[it,j-1,i+1] - u[it,j+1,i-1]*v[it,j+1,i-1] + u[it,j-1,i-1]*v[it,j-1,i-1])/(4*dx*dy)
+
+            conv_esp_pr   = d2udx2 + d2vdy2 + 2*d2uvdxy2
+
+            p[it,j,i]   = ((dx2*dy2)/(2*(dy2 + dx2)))*(dp + conv_esp_pr - (1/re)*(d2d1dx2 + d2d1dy2) - (1/dt)*d1[it,j,i] ) 
+            #p[it,j,i]   = sbr*p[it,j,i] + (1. - sbr)*p[it+1,j,i]
+
+    for j in range(0,nj-1):
+       p[it,j,0]       = p[it,j,1]
+       p[it,j,ni-1]    = 1. 
+
+    for i in range(0,ni-1):
+        p[it,0,i]      = .75*p[it,1,i] + .25*p[it,2,i]
+        p[it,nj-1,i]   = .75*p[it,nj-2,i] + .25*p[it,nj-3,i]
 
     if it%10 == 0:
-        print(f'Dilatação: {d1}')
+        dmax=0
+        jmax=0
+        imax=0
+        for i in range(ni):
+            for j in range(nj):
+                if d1[it,j,i] > dmax:
+                    dmax = d1[it,j,i]
+                    imax = i
+                    jmax = j
+        print(f'it: {it} -- i: {imax} -- j: {jmax} -- Dilatação: {dmax}')
 
 
 breakpoint()
 
-plot_heatmap(u[-1,:,:], yi, xi)
+plot_heatmap(u[-1,:,:], xi, yi)
 plt.show()
+
